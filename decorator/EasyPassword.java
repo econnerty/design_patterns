eedc116a75090b2c9cdaaf665867499134c61867
Erik Connerty <erikc@email.sc.edu>
2022-01-26 02:45:46 -0500

    Fixed logic for easypassword so that the random number is not 0 or 100

--- decorator/EasyPassword.java
+++ decorator/EasyPassword.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class EasyPassword extends Password {
 
-    private final int MAX_RANDOM_NUMBER = 100; //Added for Magic Number Avoidance
+    private static final int MAX_RANDOM_NUMBER = 99;
+    private static final int MIN_NUMBER = 1;
     
     /**
      * Constructs an EasyPassword from a String
@@ -17,7 +18,8 @@ public class EasyPassword extends Password {
     public String getPassword() {
         Random r = new Random();
         
-        this.password = (this.password+r.nextInt(MAX_RANDOM_NUMBER+1)).replaceAll("\\s", "");
+        //This will concatenate a number from the range 1-99
+        this.password = (this.password+(r.nextInt(MAX_RANDOM_NUMBER)+MIN_NUMBER)).replaceAll("\\s", "");
         return this.password;
     }
 
