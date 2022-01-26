0dba3b45822e41df4e394a56fb59f339d7d33bd6
Erik Connerty <erikc@email.sc.edu>
2022-01-26 03:40:47 -0500

    accurate description

--- decorator/RandomCasing.java
+++ decorator/RandomCasing.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomCasing extends PasswordDecorator{
 
-    //Setting this to 2 will allow us to have a 50% chance of uppercase or lowercase. If it were changed to 3 there would be a 33% chance, and 4 would be 25% chance etc.
+    //Setting this to 2 will allow us to have a 50% chance of an uppercase. If it were changed to 3 there would be a 33% chance, and 4 would be 25% chance etc.
     private static final int MAX_RANDOM_INT = 2;
 
     /**
