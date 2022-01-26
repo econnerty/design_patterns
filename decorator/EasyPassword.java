3b25b53168059ae546dc5b0fe965d8ccff9fd6f6
Erik Connerty <erikc@email.sc.edu>
2022-01-26 03:15:14 -0500

    Fixing logic for all cases

--- decorator/EasyPassword.java
+++ decorator/EasyPassword.java
@@ -19,8 +19,11 @@ public class EasyPassword extends Password {
     public String getPassword() {
         Random r = new Random();
         
+        //Getting the number
+        int randomNumber = r.nextInt((MAX_RANDOM_NUMBER+1)-MIN_NUMBER)+MIN_NUMBER;
+
         //This will concatenate a number from the range 1-99
-        this.password = (this.password+(r.nextInt((MAX_RANDOM_NUMBER+1)-MIN_NUMBER)+MIN_NUMBER)).replaceAll("\\s", "");
+        this.password = (this.password+randomNumber).replaceAll("\\s", "");
         return this.password;
     }
 
