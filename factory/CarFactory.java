cc40703a9fd6e483438c79d41a3256cf298066f1
Erik Connerty <erikc@email.sc.edu>
2022-02-02 05:23:22 -0500

    Switch statement

--- factory/CarFactory.java
+++ factory/CarFactory.java
@@ -13,18 +13,24 @@ public class CarFactory {
 
         System.out.println("Creating a " + make + " " +model);
 
-        if (type.equalsIgnoreCase("small")) {
-            car = new SmallCar(make, model);
+        switch (type) {
+
+            case "small":
+                car = new SmallCar(make, model);
+                break;
+
+            case "sedan":
+                car = new SedanCar(make, model);
+                break;
+
+            case "luxury":
+                car = new LuxuryCar(make, model);
+                break;
+
+            default:
+                return null;
         }
-        else if (type.equalsIgnoreCase("sedan")) {
-            car = new SedanCar(make, model);
-        }
-        else if (type.equalsIgnoreCase("luxury")) {
-            car = new LuxuryCar(make, model);
-        }
-        else {
-            return null;
-        }
+        
         car.assemble();
         return car;
 
